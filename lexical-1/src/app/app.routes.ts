3e2ba2ebe9ad659aee887e3abee8396d5c8e37a8
Anton.R <mail.anton.r@gmail.com>
2025-11-06 08:54:59 +0200

    wip

--- lexical-1/src/app/app.routes.ts
+++ lexical-1/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { LexicalEditorComponent } from './lexical-editor/lexical-editor.component';
 import { TiptapEditorComponent } from './tiptap-editor/tiptap-editor.component';
+import { FlogeezTiptapEditorComponent } from './flogeez-tiptap-editor/flogeez-tiptap-editor.component';
 
 export const routes: Routes = [
   {
@@ -10,5 +11,9 @@ export const routes: Routes = [
   {
     path: 'tiptap',
     component: TiptapEditorComponent
+  },
+  {
+    path: 'flogeez-tiptap',
+    component: FlogeezTiptapEditorComponent
   }
 ];
---
+++ lexical-1/src/app/flogeez-tiptap-editor/flogeez-tiptap-editor.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-flogeez-tiptap-editor',
+  imports: [],
+  templateUrl: './flogeez-tiptap-editor.component.html',
+  styleUrl: './flogeez-tiptap-editor.component.scss'
+})
+export class FlogeezTiptapEditorComponent {
+
+}
