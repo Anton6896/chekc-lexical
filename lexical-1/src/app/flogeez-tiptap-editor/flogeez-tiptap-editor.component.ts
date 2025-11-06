cc0a07fa36e95e61d77dcdfe9e8c1a3505bc96c8
Anton.R <mail.anton.r@gmail.com>
2025-11-06 09:02:41 +0200

    wip

--- lexical-1/src/app/flogeez-tiptap-editor/flogeez-tiptap-editor.component.ts
+++ lexical-1/src/app/flogeez-tiptap-editor/flogeez-tiptap-editor.component.ts
@@ -1,11 +1,42 @@
 import { Component } from '@angular/core';
 
+import { FormControl, ReactiveFormsModule } from "@angular/forms";
+
+
+import {
+  AngularTiptapEditorComponent,
+  DEFAULT_TOOLBAR_CONFIG,
+  DEFAULT_BUBBLE_MENU_CONFIG,
+} from "@flogeez/angular-tiptap-editor";
+
 @Component({
   selector: 'app-flogeez-tiptap-editor',
-  imports: [],
+  imports: [
+    AngularTiptapEditorComponent, ReactiveFormsModule
+  ],
   templateUrl: './flogeez-tiptap-editor.component.html',
   styleUrl: './flogeez-tiptap-editor.component.scss'
 })
 export class FlogeezTiptapEditorComponent {
+  content = "<h1>Welcome!</h1><p>Start editing...</p>";
+
+  // Use default configurations as base
+  toolbarConfig = {
+    ...DEFAULT_TOOLBAR_CONFIG,
+    clear: true, // Add clear button
+  };
+
+  bubbleMenuConfig = {
+    ...DEFAULT_BUBBLE_MENU_CONFIG,
+    table: true, // Enable table bubble menu
+  };
+
+  slashCommandsConfig = {
+    commands: [], // Will be populated by the library
+  };
+
+  onContentChange(newContent: string) {
+    this.content = newContent;
+  }
 
 }
